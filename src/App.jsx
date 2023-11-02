65a7e644deb9bb1b6f3d11e02155222cd0004130
Georgiy Garenov <g_garenov@outlook.com>
2023-11-02 19:02:25 +0200

    Make a header component and import it in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <h1>My first React App</h1>
-      </div>
+    <div>
+      <Header />
+      
       <h1>Vite + React = mashinki</h1>
       </div>
         
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div>
+        <h1>My first React App</h1>
+      </div>
+    )
+}
