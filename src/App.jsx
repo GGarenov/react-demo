ab447557a2690f37446437c173aeb04e98fca073
Georgiy Garenov <g_garenov@outlook.com>
2023-11-02 18:33:12 +0200

    All the images were deleted and only the headings were left

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,10 @@ function App() {
     <div className="App">
       <div>
         <h1>My first React App</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-       
       </div>
       <h1>Vite + React = mashinki</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-    
-    </div>
+        
   )
 }
 
